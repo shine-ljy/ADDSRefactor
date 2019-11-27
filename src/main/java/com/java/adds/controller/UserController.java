fb391474f6124891505be11a000250667810e692
ljy <2807513230@qq.com>
2019-11-27 16:39:20 +0800

    修改登录，401//用户不存在，402//密码错误，200//登录成功
    返回值为UserEntity

--- src/main/java/com/java/adds/controller/UserController.java
+++ src/main/java/com/java/adds/controller/UserController.java
@@ -6,7 +6,6 @@ import com.java.adds.entity.UserEntity;
 import com.java.adds.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
@@ -21,15 +20,16 @@ public class UserController {
      * @return
      */
     @PostMapping("login")
-    public Integer login(HttpServletResponse httpServletResponse, @RequestBody LoginVO loginVO)
+    public UserEntity login(HttpServletResponse httpServletResponse, @RequestBody LoginVO loginVO)
     {
         UserEntity userEntity = userService.login(loginVO.getLogin_name());
         if(userEntity==null)
             httpServletResponse.setStatus(401);  //用户不存在
         else if(!userEntity.getPassword().equals(loginVO.getPassword()))
             httpServletResponse.setStatus(402);  //密码错误
-
-        return userEntity.getType(); //返回登录角色
+        else
+            httpServletResponse.setStatus(200);  //登录成功
+        return userEntity;
     }
 
     /**ljy
