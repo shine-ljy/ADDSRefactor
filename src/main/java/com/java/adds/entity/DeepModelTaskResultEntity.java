3310e3f08a69e12481165d1c81754d36179e95ed
Liam <xueliang795m@163.com>
2020-03-29 02:29:22 +0800

    修改了深度学习任务结果的接口

--- src/main/java/com/java/adds/entity/DeepModelTaskResultEntity.java
+++ src/main/java/com/java/adds/entity/DeepModelTaskResultEntity.java
@@ -1,9 +1,13 @@
 package com.java.adds.entity;
 
+import lombok.Data;
 
+@Data
 public class DeepModelTaskResultEntity {
-    private Integer id;
-    private Integer taskId;
+    private Long id;
+    private Long taskId;
+    private Long modelId;
+    private String modelName;
     private String ndcg1;
     private String ndcg3;
     private String ndcg5;
@@ -16,116 +20,4 @@ public class DeepModelTaskResultEntity {
     private String precision3;
     private String precision5;
     private String precision10;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Integer taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getNdcg1() {
-        return ndcg1;
-    }
-
-    public void setNdcg1(String ndcg1) {
-        this.ndcg1 = ndcg1;
-    }
-
-    public String getNdcg3() {
-        return ndcg3;
-    }
-
-    public void setNdcg3(String ndcg3) {
-        this.ndcg3 = ndcg3;
-    }
-
-    public String getNdcg5() {
-        return ndcg5;
-    }
-
-    public void setNdcg5(String ndcg5) {
-        this.ndcg5 = ndcg5;
-    }
-
-    public String getNdcg10() {
-        return ndcg10;
-    }
-
-    public void setNdcg10(String ndcg10) {
-        this.ndcg10 = ndcg10;
-    }
-
-    public String getMap() {
-        return map;
-    }
-
-    public void setMap(String map) {
-        this.map = map;
-    }
-
-    public String getRecall3() {
-        return recall3;
-    }
-
-    public void setRecall3(String recall3) {
-        this.recall3 = recall3;
-    }
-
-    public String getRecall5() {
-        return recall5;
-    }
-
-    public void setRecall5(String recall5) {
-        this.recall5 = recall5;
-    }
-
-    public String getRecall10() {
-        return recall10;
-    }
-
-    public void setRecall10(String recall10) {
-        this.recall10 = recall10;
-    }
-
-    public String getPrecision1() {
-        return precision1;
-    }
-
-    public void setPrecision1(String precision1) {
-        this.precision1 = precision1;
-    }
-
-    public String getPrecision3() {
-        return precision3;
-    }
-
-    public void setPrecision3(String precision3) {
-        this.precision3 = precision3;
-    }
-
-    public String getPrecision5() {
-        return precision5;
-    }
-
-    public void setPrecision5(String precision5) {
-        this.precision5 = precision5;
-    }
-
-    public String getPrecision10() {
-        return precision10;
-    }
-
-    public void setPrecision10(String precision10) {
-        this.precision10 = precision10;
-    }
 }
--- src/main/java/com/java/adds/mapper/DeepModelTaskResultMapper.java
+++ src/main/java/com/java/adds/mapper/DeepModelTaskResultMapper.java
@@ -1,17 +1,75 @@
 package com.java.adds.mapper;
 
-
 import com.java.adds.entity.DeepModelTaskResultEntity;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Mapper
 @Repository
 public interface DeepModelTaskResultMapper {
+
+    /**
+     * QXL
+     * Get Model Evaluation Task Result
+     * 注：目前使用的是临时数据表 task_result_evaluation_temp
+     * @param taskId task id
+     * @return A DeepModelTaskResultEntity
+     */
+    @Select("SELECT r.*, m.model_name " +
+            "FROM " +
+            "task_result_evaluation_temp r LEFT JOIN deep_model m " +
+            "ON r.model_id=m.id " +
+            "WHERE r.task_id=#{taskId}")
+    @Results(id = "taskResultMap", value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "taskId", column = "task_id"),
+            @Result(property = "modelId", column = "model_id"),
+            @Result(property = "modelName", column = "model_name"),
+            @Result(property = "map", column = "map"),
+            @Result(property = "precision3", column = "p@3"),
+            @Result(property = "precision5", column = "p@5"),
+            @Result(property = "recall3", column = "r@3"),
+            @Result(property = "recall5", column = "r@5"),
+            @Result(property = "ndcg3", column = "n@3"),
+            @Result(property = "ndcg5", column = "n@5")
+    })
+    DeepModelTaskResultEntity getModelEvaluationTaskResult(@Param("taskId") Long taskId);
+
+    /**
+     * QXL
+     * Get Auto Selection Task Results
+     * 注：目前使用的是临时数据表 task_result_selection_temp
+     * @param taskId task id
+     * @return A DeepModelTaskResultEntity ArrayList
+     */
+    @Select("SELECT r.*, m.model_name " +
+            "FROM " +
+            "task_result_selection_temp r LEFT JOIN deep_model m " +
+            "ON r.model_id=m.id " +
+            "WHERE r.task_id=#{taskId}")
+    @ResultMap(value = "taskResultMap")
+    ArrayList<DeepModelTaskResultEntity> getAutoSelectionTaskResults(@Param("taskId") Long taskId);
+
+    /**
+     * QXL
+     * Get Knowledge Exploration Task Results
+     * 注：目前使用的是临时数据表 task_result_exploration_temp
+     * @param taskId task id
+     * @return A DeepModelTaskResultEntity ArrayList
+     */
+    @Select("SELECT r.*, m.model_name " +
+            "FROM " +
+            "task_result_exploration_temp r LEFT JOIN deep_model m " +
+            "ON r.model_id=m.id " +
+            "WHERE r.task_id=#{taskId}")
+    @ResultMap(value = "taskResultMap")
+    ArrayList<DeepModelTaskResultEntity> getKnowledgeExplorationTaskResults(@Param("taskId") Long taskId);
+
     /**ljy
      * 插入模型运行结果
      * @return
      */
-    public Integer insertDeepModelTaskResult(@Param("deepModelTaskResultEntity") DeepModelTaskResultEntity deepModelTaskResultEntity);
+    Long insertDeepModelTaskResult(@Param("deepModelTaskResultEntity") DeepModelTaskResultEntity deepModelTaskResultEntity);
 }
