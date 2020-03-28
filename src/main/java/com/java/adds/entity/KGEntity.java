d04043ae6c0515208d5dea5c07c58838ee2cb71e
Liam <xueliang795m@163.com>
2020-03-29 02:22:23 +0800

    新增Neo4j驱动配置、KG模块

--- src/main/java/com/java/adds/entity/KGEntity.java
+++ src/main/java/com/java/adds/entity/KGEntity.java
@@ -1,41 +1,16 @@
 package com.java.adds.entity;
 
+import lombok.Data;
 
+/**
+ * KG Entity
+ * @author QXL
+ */
+@Data
 public class KGEntity {
     private Long id;
-    private Long user_id;
-    private String kg_path;
-    private String kg_name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getKg_path() {
-        return kg_path;
-    }
-
-    public void setKg_path(String kg_path) {
-        this.kg_path = kg_path;
-    }
-
-    public String getKg_name() {
-        return kg_name;
-    }
-
-    public void setKg_name(String kg_name) {
-        this.kg_name = kg_name;
-    }
+    private Long userId;
+    private String name;
+    private String desc;
+    private String filePath;
 }
--- src/main/java/com/java/adds/mapper/KGMapper.java
+++ src/main/java/com/java/adds/mapper/KGMapper.java
@@ -1,26 +1,40 @@
 package com.java.adds.mapper;
 
-
-import com.java.adds.entity.DataSetsEntity;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import com.java.adds.entity.KGEntity;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+/**
+ * KG Mapper
+ * @author QXL
+ */
 @Mapper
 @Repository
 public interface KGMapper {
-    /**ljy
-     * 医生上传知识图谱
-     * @return
-     */
-    public Long uploadKG(@Param("uId") Long doctorId, @Param("fileName") String fileName, @Param("filePath") String filePath);
 
-    /**ljy
-     * 医生获获取知识图谱
-     * @return
+    /**
+     * Upload Knowledge-Graph file
+     * @param userId user's id
+     * @return A KGEntity ArrayList
      */
-    public ArrayList<DataSetsEntity> getKGS(@Param("uId") Long doctorId);
+    @Select("SELECT * FROM kg_upload WHERE user_id=#{userId}")
+    @Results(id = "kgMap", value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "desc", column = "kg_desc"),
+            @Result(property = "filePath", column = "file_path")
+    })
+    ArrayList<KGEntity> getKGListByUserId(@Param("userId") Long userId);
 
+    /**
+     * Upload Knowledge-Graph file
+     * @param kgEntity KG file info
+     */
+    @Insert("INSERT INTO kg_upload(user_id, name, kg_desc, file_path) " +
+            "values(#{kgEntity.userId}, #{kgEntity.name}, #{kgEntity.desc}, #{kgEntity.filePath})")
+    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "kgEntity.id")
+    void uploadKGFile(@Param("kgEntity") KGEntity kgEntity);
 }
